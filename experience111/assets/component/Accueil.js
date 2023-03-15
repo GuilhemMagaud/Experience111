bf3bf7c68c3a134f13763e7cd4c773785eb30854
GuilhemMagaud <guilhem.magaud@orange.fr>
2023-03-15 12:06:51 +0100

    Create Accueil.js
    test modif

---
+++ experience111/assets/component/Accueil.js
@@ -0,0 +1,12 @@
+import React, {Component, useState} from 'react';
+
+
+function Acceuil(){
+    return(
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default Acceuil;
