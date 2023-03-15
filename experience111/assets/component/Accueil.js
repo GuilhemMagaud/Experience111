1c192ce394ea0c112e9357e1d1d0a9a11a4ea75f
GuilhemMagaud <guilhem.magaud@orange.fr>
2023-03-15 17:51:06 +0100

    footer responsive not completed

--- experience111/assets/component/Accueil.js
+++ experience111/assets/component/Accueil.js
@@ -1,10 +1,13 @@
 import React, {Component, useState} from 'react';
+import { Navbar } from './Header/Navbar';
+import { BasPage } from './Footer/BasPage';
 
 
 function Acceuil(){
     return(
         <div>
             <p>hello world</p>
+            <footer><BasPage /></footer>
         </div>
     )
 }
