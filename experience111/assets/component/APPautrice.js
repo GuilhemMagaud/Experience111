f729dfa7f02e9786affbfbac32bec6361e455887
Chaimaabalk <balkassechaimaa8@gmail.com>
2023-03-18 21:18:54 +0100

    A propos l'autrice

--- experience111/assets/component/APPautrice.js
+++ experience111/assets/component/APPautrice.js
@@ -1,1 +1,17 @@
-import React, {Component, useState} from 'react';
+import Autrice from '../images/Photo-auteur-titi.png';
+export function APPautrice (){
+    return (
+        <div>
+            <img src={Autrice} alt="Photo Othilie NARDIN" />
+            <p>
+            Othilie Nardin est née à Nice en 2003. Elle a commencé à écrire ses premières histoires fantaisies à l’âge de onze ans.
+
+Passionnée par la littérature de science-fiction, c’est à travers un projet scolaire qu’elle décide d’autopublier son premier roman.
+
+Elle s’inspire de ses rêves et cauchemars pour écrire des histoires originales et pleines de rebondissements.
+            </p>
+        </div>
+       
+
+    )
+}
